f34b08252d0ee8e93d2721e3cc0eb3e24096c162
Hideki NAKAJIMA <heitler@gmail.com>
2024-08-17 00:39:05 +0700

    Add files via upload

--- Python/MPW.py
+++ Python/MPW.py
@@ -16,7 +16,7 @@ build = 'full'
 period = 230 # Undulator period (mm)
 gap = 15 # Udulator gap (mm)
 #n_periods = 4 # Number of periods
-n_poles = 5 # Number of poles
+n_poles = 1 # Number of poles
 
 # Magnetization errors
 sdr = 0 # standard deviation of amplitude errors (%/100)
@@ -27,13 +27,13 @@ mat = 'ndfeb' # Should be 'cryo', 'ndfeb' or 'smco'
 # Build a parameter object according to the material specification
 if mat == 'smco':
     # For Sm2Co17 magnets
-    params = rid.HybridWigParam(period, n_poles, sdr, sdt, gap, ext_pole=[9.5, 0], mag_mat='sm2co17', br=1.1, wig_build=build)
+    params = rid.HybridWigParam(period, n_poles, sdr, sdt, gap, mag_mat='sm2co17', br=1.1, wig_build=build)
 elif mat == 'ndfeb':
     # For room temperature NdFeB (Br may be a bit optimistic)
     params = rid.HybridWigParam(period, n_poles, sdr, sdt, gap, ext_pole=[9.5, 0], mag_mat='ndfeb', br=1.20, wig_build=build)
 elif mat == 'cryo':
     # For PrFeB magnets at 80 K and Vanadium Permadur poles
-    params = rid.HybridWigParam(period, n_poles, sdr, sdt, gap, ext_pole=[9.5, 0], mag_mat='ndfeb', br=1.6, pole_mat='fecov', wig_build=build)
+    params = rid.HybridWigParam(period, n_poles, sdr, sdt, gap, mag_mat='ndfeb', br=1.6, pole_mat='fecov', wig_build=build)
 
 
 # Build and solve
@@ -47,49 +47,48 @@ und.load(fileName)
 """
 #und.print_wavelength(e=1.2, n=1, theta=0)
 
-# Export the parameters and model
-#und.save(fileName) # save parameters and model
-# Save VTK
-und.exportGeometryToVTK(fileName)
+# Export the geometric parameters
+#und.save(fileName) 
+
+# Save VTK (geometrical model)
+#und.exportGeometryToVTK(fileName)
 """
 # Plot geometry
 if sys.platform == "win32":
     und.plot_geo('EdgeLines->True')
     #'EdgeLines->True|False,Faces->True|False,Axes->True|False'
 else:
-    # Plot VTK by PyVISTA
+    # Plot VTK by PyVISTA (save vtk first)
     grid = pv.read(fileName+'.vtk')
     grid.plot(cmap='viridis',show_scalar_bar=False,show_axes=False,show_edges=True,window_size = [1000, 580],lighting=True,component=1)
 """
-# Plot results (need to plot geometry above)
+# Plot field (need to plot geometry above)
 #und.plot_field(xyz_end=[0, 1000, 0], xyz_start=[0, -1000, 0], n=1000, b='bz', x_axis='d', plot_show=True, plot_title='field_y')
-"""
+#und.plot_field([50, 0, 0], [-50, 0, 0], n=1000, b='bz', x_axis='d', plot_show=True, plot_title='field_x')
+
 # Plot 3D vector field by pyvista
 #und.plot_vector_field(-100,101,5,-200,201,5,-15,16,5,3,plot_save=True, plot_title=fileName)
-#und.plot_field([50, 0, 0], [-50, 0, 0], n=1000, b='bz', x_axis='d', plot_show=True, plot_title='field_x')
-#und.plot_field_int(xyz_end=[50, 0, 0], xyz_start=[-50, 0, 0], dir_int=[0, 1, 0], n=1000, b='bz', x_axis='d', plot_show=True, plot_title='field_int')
-"""
-# und.plot_traj(e=1.2, init_cond=[0, 0, 0, 0], y_range=None, n_points=1000, x_or_z='x', plot_show=True, plot_title='traj_y')
-"""
-und.plot_field(xyz_end=[0, 1000, 0], xyz_start=[0, -1000, 0], n=1000, b='bz', x_axis='d', plot_show=True, plot_title='field_y')
-und.plot_field([50, 0, 0], [-50, 0, 0], n=1000, b='bz', x_axis='d', plot_show=True, plot_title='field_x')
-und.plot_traj(e=1.2, init_cond=[0, 0, 0, 0], y_range=None, n_points=1000, x_or_z='x', plot_show=True, plot_title='traj_y')
-und.plot_field_int(xyz_end=[0, 2000, 0], xyz_start=[0, -2000, 0], dir_int=[1, 0, 0], n=1000, b='bz', x_axis='d', plot_show=True, plot_title='field_int')
-und.plot_field_int(xyz_end=[200, 0, 0], xyz_start=[-200, 0, 0], dir_int=[0, 1, 0], n=1000, b='bz', x_axis='d', plot_show=True, plot_title='field_int')
-und.print_wavelength(e=1.2, n=1, theta=0)
-und.plot_vector_field(-100,101,5,-200,201,5,-15,16,5,3,plot_save=True, plot_title=fileName)
-"""
-# setup force calc# calc force
+
+# Plot field integral
+#und.plot_field_int(xyz_end=[0, 2000, 0], xyz_start=[0, -2000, 0], dir_int=[1, 0, 0], n=1000, b='bz', x_axis='d', plot_show=True, plot_title='field_int')
+#und.plot_field_int(xyz_end=[200, 0, 0], xyz_start=[-200, 0, 0], dir_int=[0, 1, 0], n=1000, b='bz', x_axis='d', plot_show=True, plot_title='field_int')
+
+# Plot beam trajectory
+#und.plot_traj(e=1.2, init_cond=[0, 0, 0, 0], y_range=None, n_points=1000, x_or_z='x', plot_show=True, plot_title='traj_y')
+
+# Magnetic force (unknown)
 #und.force(normal_plane=None, point=None)
 #und.test_obj_export_vtk()
 #und.force2(k=[1,1,4])
 
-list_save  = []
+
+# Dependence of field and field integral upon gap and ext pole
+list_save  = [] # to be saved
 #list_gap = [5,10,15,20,25,30,35,40,45,50]
-list_gap = [15]
-list_epole = [8,8.5,9,9.5,10,10.5,11]
-list_field = []
-#for gap in [800,400,200,150,110,90,70,50,40,30,25,20,15,10,5,3]:
+list_gap = [15] # undulator gap
+list_epole = [8,8.5,9,9.5,10,10.5,11] # ending pole length
+list_field = [] # peak field
+
 for gap in list_gap:
     for epole in list_epole:
         params = rid.HybridWigParam(period, n_poles, sdr, sdt, gap, ext_pole=[epole, 0], mag_mat='ndfeb', br=1.2, wig_build=build)
@@ -104,6 +103,7 @@ list_save.append(list_field)
 np.savetxt(fileName+"fld_int_epole.csv",np.transpose(list_save),header='ME/eV,test',delimiter =",")
 
 """
+# Studies on standard errors
 list_sd =[]
 list_field_all =[]
 for i in range(0, 11, 1):
